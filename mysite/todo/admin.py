c08749be38accf509dc3f36479b14d38883640d2
hamatsumoto <kmdai0219@gmail.com>
2018-10-26 21:50:08 +0900

    create todolist app ver 1.0

--- mysite/todo/admin.py
+++ mysite/todo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+#レコード表示機能
+class PostAdmin(admin.ModelAdmin):
+    pass
+    list_display = ('message','create_date',)
+    list_display_links = ('message','create_date',)
+
+admin.site.register(Post,PostAdmin)
